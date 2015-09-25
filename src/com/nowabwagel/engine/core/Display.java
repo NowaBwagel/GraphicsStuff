bd86b2d2a5561db73d2727cf2ed826b180cd0c76
NowaBwagel <noah.bergl@gmail.com>
2015-09-25 08:29:30 -0500

    More display

--- src/com/nowabwagel/engine/core/Display.java
+++ src/com/nowabwagel/engine/core/Display.java
@@ -1,11 +1,15 @@
 package com.nowabwagel.engine.core;
 
-import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-
 import org.lwjgl.Sys;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Display {
 
@@ -14,20 +18,80 @@ public class Display {
 
 	private long window;
 
+	private String title;
+	private int width;
+	private int height;
+
+	public Display(String title, int width, int height) {
+		this.title = title;
+		this.width = width;
+		this.height = height;
+	}
+
 	public void start() {
 		System.out.println("LWJGL: " + Sys.getVersion());
-	}
-	
-	public void run(){
-		glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
+
+		try {
+			init();
+			run();
+
+			glfwDestroyWindow(window);
+			keyCallback.release();
+		} finally {
+			glfwTerminate();
+			errorCallback.release();
+		}
+
 	}
 
 	private void init() {
+		glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
 
+		if (glfwInit() != GL11.GL_TRUE)
+			throw new IllegalStateException("Unable to initialize GLFW");
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+		if (window == NULL)
+			throw new RuntimeException("Failed to create GLFW window");
+
+		// TODO: move glfwKeyCallback to own class
+		glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
+			@Override
+			public void invoke(long window, int key, int scancode, int action,
+					int mods) {
+				if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+					glfwSetWindowShouldClose(window, GL_TRUE);
+			}
+		});
+
+		ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+		glfwSetWindowPos(window, (GLFWvidmode.width(vidmode) - width) / 2,
+				(GLFWvidmode.height(vidmode) - height) / 2);
+
+		glfwMakeContextCurrent(window);
+		glfwSwapInterval(1);
+		glfwShowWindow(window);
 	}
 
-	public void loop() {
+	public void run() {
+		GL.createCapabilities();
 
+		glClearColor(1f, 0f, 0f, 0f);
+
+		while (glfwWindowShouldClose(window) == GL_FALSE) {
+			render();
+			glfwPollEvents();
+		}
+	}
+
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glfwSwapBuffers(window);
 	}
 
 	public long getWindow() {
