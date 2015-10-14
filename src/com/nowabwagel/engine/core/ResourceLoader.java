79c0b921bac8d6a7c59601a1ce4662bce7f98d5e
NowaBwagel <noah.bergl@gmail.com>
2015-10-14 09:32:43 -0500

    nothing

--- src/com/nowabwagel/engine/core/ResourceLoader.java
+++ src/com/nowabwagel/engine/core/ResourceLoader.java
@@ -2,6 +2,9 @@ package com.nowabwagel.engine.core;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+
+import com.nowabwagel.engine.core.math.Vector3f;
 
 public class ResourceLoader {
 	public static final String SHADER_LOCATION = "./res/shaders/";
@@ -16,7 +19,8 @@ public class ResourceLoader {
 		BufferedReader shaderReader;
 
 		try {
-			shaderReader = new BufferedReader(new FileReader(SHADER_LOCATION + fileName));
+			shaderReader = new BufferedReader(new FileReader(SHADER_LOCATION
+					+ fileName));
 			String line;
 			while ((line = shaderReader.readLine()) != null) {
 				shaderSource.append(line).append("\n");
@@ -41,16 +45,36 @@ public class ResourceLoader {
 
 	private static Mesh loadOBJ(String fileName) {
 		Mesh mesh = new Mesh();
+
+		ArrayList<Vector3f> positions = new ArrayList<Vector3f>();
+		ArrayList<Integer> indices = new ArrayList<Integer>();
+
 		BufferedReader meshReader;
 		try {
-			meshReader = new BufferedReader(new FileReader(MESH_LOCATION + fileName));
+			meshReader = new BufferedReader(new FileReader(MESH_LOCATION
+					+ fileName));
 			String line;
-			
+
+			while ((line = meshReader.readLine()) != null) {
+				String[] tokens = line.split(" ");
+
+				if (tokens.length == 0 || tokens[0].equals("#"))
+					continue;
+
+				if (tokens[0].equals("v")) {
+					positions.add(new Vector3f(Float.valueOf(tokens[1]), Float
+							.valueOf(tokens[2]), Float.valueOf(tokens[3])));
+				}
+				else if(tokens[0].equals("f"));//FIXME: lol i broken
+
+			}
+
+			meshReader.close();
 		} catch (Exception e) {
 			System.out.println(e);
 			System.exit(1);
 		}
-		
+
 		return mesh;
 	}
 }
